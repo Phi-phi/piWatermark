ed0fb021ee2b181f24dd97ed5e83ef1ad7154179
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2017-01-11 03:16:26 +0900

    remove time limit

--- server.py
+++ server.py
@@ -4,6 +4,7 @@ import struct
 import time
 import picamera
 
+
 # Connect a client socket to my_server:8000 (change my_server to the
 # hostname of your server)
 socket = socket.socket()
@@ -24,23 +25,18 @@ try:
         # temporarily (we could write it directly to connection but in this
         # case we want to find out the size of each capture first to keep
         # our protocol simple)
-        start = time.time()
         stream = io.BytesIO()
-        while True:
-            for foo in camera.capture_continuous(stream, 'jpeg'):
-                # Write the length of the capture to the stream and flush to
-                # ensure it actually gets sent
-                connection.write(struct.pack('<L', stream.tell()))
-                connection.flush()
-                # Rewind the stream and send the image data over the wire
-                stream.seek(0)
-                connection.write(stream.read())
-                # If we've been capturing for more than 30 seconds, quit
-                if time.time() - start > 30:
-                    break
-                # Reset the stream for the next capture
-                stream.seek(0)
-                stream.truncate()
+        for foo in camera.capture_continuous(stream, 'jpeg'):
+            # Write the length of the capture to the stream and flush to
+            # ensure it actually gets sent
+            connection.write(struct.pack('<L', stream.tell()))
+            connection.flush()
+            # Rewind the stream and send the image data over the wire
+            stream.seek(0)
+            connection.write(stream.read())
+            # Reset the stream for the next capture
+            stream.seek(0)
+            stream.truncate()
     # Write a length of zero to the stream to signal we're done
     connection.write(struct.pack('<L', 0))
 finally:
