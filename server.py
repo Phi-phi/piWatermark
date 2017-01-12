9998b130ae2ea505600b687e8f55f09c76e14a56
Kazuaki Masuda <kame.kazu.0213.masumerry.0112@gmail.com>
2017-01-12 19:05:31 +0900

    downscale

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ socket.listen(0)
 connection = socket.accept()[0].makefile('wb')
 try:
     with picamera.PiCamera() as camera:
-        camera.resolution = (960, 720)
+        camera.resolution = (640, 480)
         camera.framerate = 60
         # Start a preview and let the camera warm up for 2 seconds
         camera.start_preview()
